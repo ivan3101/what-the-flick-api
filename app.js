b6ef2520d764342749acb8e2de364c8fbb14a033
Ivan De Menezes <ivandmenezes@gmail.com>
2018-03-25 13:55:44 -0400

    Initialized app

---
+++ app.js
@@ -0,0 +1,16 @@
+const Morgan = require('morgan');
+const BodyParser = require('body-parser');
+const Cors = require('cors');
+const Helmet = require('helmet');
+const Express = require('express');
+const App = Express();
+const PORT = process.env.PORT || 3000;
+
+App.use(Helmet());
+App.use(Cors());
+if (App.get('ENV') === 'development') App.use(Morgan('dev'));
+else App.use(Morgan('short'));
+App.use(BodyParser.urlencoded({extended: false}));
+App.use(BodyParser.json());
+
+App.listen(() => console.log(`API is running in port: ${PORT}`));
