70fce1f6f1c3dda9748f57e90399f5853c6ed01f
Ivan De Menezes <ivandmenezes@gmail.com>
2018-03-25 14:48:25 -0400

    User route

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,9 +2,11 @@ const Express = require('express');
 const Router = Express.Router();
 const ArticleRoutes = require('./article');
 const AuthRoutes = require('./auth');
+const UserRoutes = require('./user');
 
 Router
     .use('/auth', AuthRoutes)
+    .use('/users', UserRoutes)
     .use('/articles', ArticleRoutes);
 
 module.exports = Router;
